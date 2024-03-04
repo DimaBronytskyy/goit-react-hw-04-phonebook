9afbed2b7caad45855bd9ca64217c9dea1dcaf37
DimaBronytskyy <138112501+DimaBronytskyy@users.noreply.github.com>
2024-03-04 23:30:04 +0200

    push

---
+++ src/components/Phonebook/ContactFiltr/ContactFiltr.jsx
@@ -0,0 +1,15 @@
+import styles from './contact-filtr.module.css';
+
+const FilterInput = ({ onChange, value }) => {
+  return (
+    <input
+      onChange={onChange}
+      name="filter"
+      placeholder="Search"
+      className={styles.input}
+      value={value}
+    />
+  );
+};
+
+export default FilterInput;
